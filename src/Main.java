6325338610a943692b7c40f5923c80a3613eb090
nirajti <Nirajtiwari329@gmail.com>
2022-06-07 10:56:49 +0530

    branch 'issue1' first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 
 public class Main {
+	public static greet() {
+		System.out.println("good morning maam");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("this is main class");
